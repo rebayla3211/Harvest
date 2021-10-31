57868801e217674285e9fea09bf847293ad191ff
Earl Batenga <61070797+Elchupappi@users.noreply.github.com>
2021-11-01 00:06:19 +0800

    1stCommit-Earl
    Improvements

---
+++ New Unity Project/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ButtonStart(){
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/LevellingAndScoring.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevellingAndScoring: MonoBehaviour
+{
+    public GameObject pickup;
+    public GameObject WinText;
+    public Text Scoretext;
+    bool Ended;
+    int MaxApple = 0;
+    int Apple = 0;
+
+
+    void Update()
+    {
+        if (Ended == true)
+        {
+            Destroy(pickup);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (Apple == MaxApple - 1 && other.tag == "Apples")
+        {
+            Ended = true;
+            Apple += 1;
+            Destroy(other);
+            Scoretext.text = "Collected Apples: " + Apple + "/"+ MaxApple;
+            WinText.SetActive(true);
+        }
+        else if (Apple <= MaxApple && other.tag == "Apples")
+        {
+            Apple += 1;
+            Scoretext.text = "Collected Apples: " + Apple + "/"+ MaxApple;
+            Destroy(other);
+        }
+    }
+    public void LevelOne(){
+        MaxApple = 3;
+        Scoretext.text = "Collected Apples: 0" + "/"+ MaxApple;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void LevelTwo(){
+        MaxApple = 6;
+        Scoretext.text = "Collected Apples: 0" + "/"+ MaxApple;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void LevelThree(){
+        MaxApple = 12;
+        Scoretext.text = "Collected Apples: 0" + "/"+ MaxApple;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
